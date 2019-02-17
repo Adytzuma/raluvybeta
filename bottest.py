c5f44f5b4af8a136f8268b4c005a45b03a58d2a5
raluvy95 <43247023+raluvy95@users.noreply.github.com>
2019-02-17 13:52:23 +0200

    Update bottest.py

--- bottest.py
+++ bottest.py
@@ -18,7 +18,7 @@ bot = commands.Bot(command_prefix=';;')
 logging.basicConfig(level='INFO')
 bot.load_extension("cogs.admin")
 bot.load_extension("cogs.youtube")
-#bot.load_extension("cogs.sound")
+bot.load_extension("cogs.sound")
 bot.load_extension("cogs.music")
 
 @bot.event
@@ -82,86 +82,6 @@ async def status(ctx, member: discord.Member=None):
     await ctx.send(embed=embed)
         
 
-
-                     
-"""                    
-
-
-@bot.event
-async def on_member_join(member):
-     with open("welcome.json", "r") as f:
-        data = json.load(f)
-     welcome = data['message'].replace("{server}", member.guild.name).replace("{user}", member.mention).replace("{membercount}", str(member.guild.member_count)) 
-     try:
-        if data['guild'] == member.guild.id:
-             await bot.get_guild(data['guild']).get_channel(data['channel']).send(welcome)
-        else:
-             return
-     except:
-        pass
-
-     
-@bot.command(aliases=["welcomemessage", "welcomemsg", "joinmsg", "joinmessage", "welcome-message", "join-message", "welcome-msg", "join-msg"])
-@commands.has_permissions(manage_guild=True)
-async def welcome(ctx, *, message=None):
-     error = "**Please use `,welcome <#channel> <message>`\n\n{user} = mention user\n{server} = server name\n{membercount} = member count**"
-     if message is None:
-        return await ctx.send(error)
-     try:
-        with open("welcome.json", "r") as f:
-             read = json.load(f)
-        if read['guild'] == ctx.guild.id:
-             if read['guild']['channel'] == message.channel.id:
-                await ctx.send(f"I'm on {message.channel.id}. Did you know? Type `--remove` for remove it and recreate it")
-        else:
-          with open("welcome.json", "w") as f:
-             json.dump(data, f)
-             data = {}
-             data['guild'] = ctx.guild.id
-             data['guild']['channel'] = message.channel.id
-             data['guild']['message'] = message
-             await ctx.send(f"**:white_check_mark: Done! I will send it on {message.channel.mention}!** :wink:")
-     except:
-        return
-
-@bot.command()
-@commands.cooldown(1, 5, commands.BucketType.user)
-async def emoji(ctx, owo: discord.Emoji=None):
-    a = random.choice(bot.emojis)
-    if owo is None:
-      return await ctx.send(a)
-    else:
-      try:
-         uwu = bot.get_emoji(bot.guilds.emojis)
-         if uwu == owo:
-              await ctx.send(uwu)
-         else:
-              await ctx.send("**That emoji is not found on the bot! Try again with `,emoji`!**")
-      except:
-         return
-
-
-@bot.command(aliases=['nickname'])
-async def nick(ctx, member: discord.Member=None, *, uwu):
-     try:
-        if member is None:
-          member = ctx.author
-        owo = member.id
-        if ctx.author.guild_permissions == 'manage_nickname' is True:
-          await ctx.guild.get_member(owo).edit(nick=uwu)
-          return await ctx.message.add_reaction('\U00002705')
-        else:
-          if ctx.author.guild_permissions == 'change_nickname' is True:
-               await ctx.guild.get_member(ctx.author.id).edit(nick=uwu)
-               return await ctx.message.add_reaction('\U00002705')
-          else:
-               await ctx.message.add_reaction('\U0000274c')
-     except discord.Forbidden as owo:
-        return await ctx.send(f"**Ops... I can't change because:**\n`{owo}`")
-
-"""
-
-
 @bot.command()
 @commands.cooldown(1, 5, commands.BucketType.user)
 async def ping(ctx):
@@ -169,4 +89,4 @@ async def ping(ctx):
     await asyncio.sleep(1)
     await t.edit(content=f':ping_pong: | **Pong!** `{ctx.bot.latency * 1000:,.0f}MS`')
 
-bot.run('NDkyMzcxODk2MzM2Mzg0MDE4.Do-y9Q.1F7gVsNEbBYw-Ca3sDFvi4H618I')
+bot.run(os.getenv("TOKEN"))
